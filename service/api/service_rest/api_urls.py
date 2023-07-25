0ec8735c29d34b1aa43a157f1b04ed62583a9d6c
Gabriel Svetcos <gsvetcos@gmail.com>
2023-07-24 19:39:51 -0700

    Added List Technicians view

--- service/api/service_rest/api_urls.py
+++ service/api/service_rest/api_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from views import list_technicians
 
 
 urlpatterns = [
-    # path('technicians/', )
+    path('technicians/', list_technicians, name="list_technicians"),
 ]
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from .models import Technician, Appointment, AutomobileVO
+from common.json import ModelEncoder
+import json
 
 # Create your views here.
+class TechnicianListEncoder(ModelEncoder):
+    model = Technician
+    properties = ["first_name", "last_name", "employee_id"]
+
+class AppointmentListEncoder(ModelEncoder):
+    model = Appointment
+    properties = [
+        "date_time",
+        "reason",
+        "status",
+        "vin",
+        "customer",
+        "technician",
+    ]
+    encoders = {
+        "technician": TechnicianListEncoder
+    }
+
+@require_http_methods(["GET", "POST"])
+def list_technicians(request):
+    if request.method == "GET":
+        technicians = Technician.objects.all()
+        return JsonResponse(
+            {"technicians": technicians},
+            encoder=TechnicianListEncoder,
+        )
+    else:
+        try:
+            content = json.loads(request.body)
+            technician = Technician.objects.create(**content)
+            return JsonResponse(
+                technician,
+                encoder=TechnicianListEncoder,
+                safe=False
+            )
+        except:
+            response = JsonResponse(
+                {"message": "Could not create technician"}
+            )
+            response.status_code = 400
+            return response
