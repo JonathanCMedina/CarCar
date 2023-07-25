b597c509d9fd82604487828ea8d124d8749aa984
Gabriel Svetcos <gsvetcos@gmail.com>
2023-07-24 19:43:53 -0700

    edited model to make employee ID unique

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -8,7 +8,7 @@ class AutomobileVO(models.Model):
 class Technician(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    employee_id = models.CharField(max_length=100)
+    employee_id = models.CharField(max_length=100, unique=True)
 
     def get_api_url(self):
         return reverse("show_technician", kwargs={"pk": self.id})
