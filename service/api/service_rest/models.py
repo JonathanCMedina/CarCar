b1dbc7c37679fe3d5a074ed40069b91c49098741
Gabriel Svetcos <gsvetcos@gmail.com>
2023-07-25 18:02:14 -0700

    Completed Back-end(?)

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -26,7 +26,7 @@ class Appointment(models.Model):
     technician = models.ForeignKey(
         Technician,
         related_name="appointments",
-        on_delete=models.PROTECT,
+        on_delete=models.CASCADE,
         null=True
     )
 
