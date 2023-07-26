f47c1f15b45a1867909aba540fce10fd09530350
Jonathan Ceasar Medinae <Jonathan.C.Medina.x@gmail.com>
2023-07-25 19:20:29 -0700

    sales completed backend: updates to urls.py and views.py, tested insomnia functionality, all systems go

--- sales/api/sales_rest/urls.py
+++ sales/api/sales_rest/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path("customers/", api_list_customers, name="api_list_customers"),
     path("sales/", api_list_sales, name="api_list_sales"),
     path(
-        "salespeople/<int:id>/",
+        "salespeople/<int:pk>/",
         api_delete_salesperson,
         name="api_delete_salesperson"
         ),
-    path("customers/<int:id>/", api_delete_customer, name="api_delete_customer"),
-    path("sales/<int:id>/", api_delete_sale, name="api_delete_sale")
+    path("customers/<int:pk>/", api_delete_customer, name="api_delete_customer"),
+    path("sales/<int:pk>/", api_delete_sale, name="api_delete_sale")
 ]
