2e21fc027a92cf0cefc0bf2bc51db6cafc492610
Gabriel Svetcos <gsvetcos@gmail.com>
2023-07-27 12:23:47 -0700

    formatting

--- ghi/app/src/CustomerList.js
+++ ghi/app/src/CustomerList.js
@@ -1,4 +1,4 @@
-function CustomerList ({ customers })
+function CustomerList({ customers })
 {
     return (
         <>
@@ -6,26 +6,24 @@ function CustomerList ({ customers })
             <table className="table table-striped">
                 <thead>
                     <tr>
-                        <th>
-                            <td> First Name </td>
-                            <td> Last Name </td>
-                            <td> Address </td>
-                            <td> Phone Number </td>
-                        </th>
+                        <th> First Name </th>
+                        <th> Last Name </th>
+                        <th> Address </th>
+                        <th> Phone Number </th>
                     </tr>
                 </thead>
                 <tbody>
                     {customers.map(customer =>
-                        {
-                            return(
-                                <tr key= { customer.id } >
-                                    <td> { customer.first_name } </td>
-                                    <td> { customer.last_name } </td>
-                                    <td> { customer.address } </td>
-                                    <td> { customer.phone_number } </td>
-                                </tr>
-                            )
-                        })}
+                    {
+                        return (
+                            <tr key={customer.id} >
+                                <td> {customer.first_name} </td>
+                                <td> {customer.last_name} </td>
+                                <td> {customer.address} </td>
+                                <td> {customer.phone_number} </td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </>
